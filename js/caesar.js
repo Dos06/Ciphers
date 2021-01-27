d4af106660fad412495da2c1da9d9384491168f3
Dos06 <dostermorgan815@gmail.com>
2021-01-27 15:19:37 +0600

    Added Caesar Cypher decryption

--- js/caesar.js
+++ js/caesar.js
@@ -1,15 +1,14 @@
 let alphabet = "abcdefghijklmnopqrstuvwxyz";
 let fullAlphabet = alphabet + alphabet + alphabet;
 
-function runCipher(){
+function encrypt() {
     let text = document.getElementById('text').value;
     let key = document.getElementById('key').value;
     key = (key % alphabet.length);
     let result = '';
 
-    for(i = 0; i < text.length; i++){
-        let letter = text[i];
-        
+    for(i = 0; i < text.length; i++) {
+        let letter = text.charAt(i);
         let index = alphabet.indexOf(letter);
         if (index == -1){
             result += letter;
@@ -22,11 +21,45 @@ function runCipher(){
     document.getElementById('result').value = result;
 }
 
+function decrypt() {
+    let text = document.getElementById('textDecrypt').value;
+    let key = document.getElementById('keyDecrypt').value;
+    key = (key % alphabet.length);
+    let result = '';
+
+    for (i = 0; i < text.length; i++) {
+        let letter = text.charAt(i);
+        let index = alphabet.indexOf(letter);
+        let cipherIndex = index -+ key;
+
+        if (index != -1) {
+            if (cipherIndex < 0) {
+                cipherIndex = cipherIndex + 26;
+            }
+            result += alphabet.charAt(cipherIndex);
+        } else {
+            if (result == " ") {
+                result += " ";
+            } else {
+                result += letter;
+            }
+        }
+    }
+    document.getElementById('resultDecrypt').value = result;
+}
+
 $(document).ready(function() {
-    $('#text').keypress(function(){
-        setTimeout(function(){ runCipher(); }, 20);
+    $('#text').keypress(function() {
+        setTimeout(function(){ encrypt(); }, 20);
     });
-    $('#text').change(function(){
-        setTimeout(runCipher(), 20);
+    $('#text').change(function() {
+        setTimeout(encrypt(), 20);
+    });
+    
+    $('#textDecrypt').keypress(function() {
+        setTimeout(function(){ decrypt(); }, 20);
+    });
+    $('#textDecrypt').change(function() {
+        setTimeout(decrypt(), 20);
     });
 });
